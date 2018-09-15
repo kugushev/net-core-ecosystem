fb8b62b1815dfbeedeb6edff056e5b05b966a987
AleksandrKugushev <kugushew@gmail.com>
2018-09-15 14:07:18 +0300

    add project from lecture

---
+++ module21-netcore/Project/AdminConsole/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Domain;
+
+namespace AdminConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quality = 0;
+            bool visited = args.Length == 1 && int.TryParse(args[0], out quality);
+
+            var service = new AssesmentService();
+            Console.WriteLine(service.Asses(!visited, quality));
+        }
+    }
+}
---
+++ module21-netcore/Project/Domain/AssesmentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain
+{
+    public class AssesmentService
+    {
+        public int Asses(bool miss, int homeworkQuality)
+        {
+            return miss ? 0 : homeworkQuality;
+        }
+    }
+}
