026541056fd489a8c725f3d48a4c6851379aa200
AleksandrKugushev <kugushew@gmail.com>
2018-09-22 12:47:54 +0300

    add resources from lecture 23

---
+++ module23-webapi/Cats/CatsService/Controllers/CatController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CatsService.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatsService.Controllers
+{
+    [Route("api/cat")]
+    [ApiController]
+    public class CatController : ControllerBase
+    {
+        [ResponseCache(Duration =42)]
+        [HttpGet("{id}/toys/{toyId?}")]
+        public ActionResult<Toy> Get(int id, int toyId = 0)
+        {
+            if (id > 0)
+            {
+                return new Toy
+                {
+                    Id = toyId
+                };
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost("{id}/toys")]
+        public IActionResult PostToy(int id, [FromBody] Toy toy)
+        {
+            if(id > 0)
+            {
+                return Ok();
+            }
+            
+            return NotFound();
+        }
+    }
+}
---
+++ module23-webapi/Cats/CatsService/Models/Toy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatsService.Models
+{
+    public class Toy: IToy
+    {
+        [Required]
+        public int? Id { get; set; }
+
+        [StringLength(5)]
+        public string Name { get; set; }
+
+        public string Excess { get; set; }
+    }
+}
